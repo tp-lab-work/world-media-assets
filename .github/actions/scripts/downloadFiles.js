d0c12ffc4e80e70fb3851c3a96cb2d5d117371e2
tp.jp <chronokreuz@gmail.com>
2024-10-25 01:02:36 +0900

    複合アクション修正

--- .github/actions/scripts/downloadFiles.js
+++ .github/actions/scripts/downloadFiles.js
@@ -10,20 +10,18 @@ const auth = new google.auth.GoogleAuth({
 const drive = google.drive({ version: 'v3', auth });
 
 async function downloadFileByQuery() {
-  try {
-    // クエリを使用してファイルを検索
-    const res = await drive.files.list({
-      q: process.env.QUERY,
-      fields: 'files(id, name)',
-      // pageSize: 1, // 検索結果の数を制限
-    });
+  // クエリを使用してファイルを検索
+  const res = await drive.files.list({
+    q: process.env.QUERY,
+    fields: 'files(id, name)'
+  });
 
-    if (res.data.files.length === 0) {
-      console.log('No files found.');
-      return;
-    }
+  if (res.data.files.length === 0) {
+    console.log('No files found.');
+    return;
+  }
 
-    const file = res.data.files[0];
+  for (const file of res.data.files) {
     console.log(`Found file: ${file.name} (${file.id})`);
 
     // ファイルをダウンロード
@@ -42,49 +40,7 @@ async function downloadFileByQuery() {
         console.error('Error downloading file:', err);
       })
       .pipe(dest);
-  } catch (err) {
-    console.error('Error:', err);
   }
 }
 
 downloadFileByQuery();
-
-// const {google} = require('googleapis');
-// const fs = require('fs');
-// const path = require('path');
-
-// const oauth2Client = new google.auth.OAuth2(
-//   process.env.GOOGLE_CLIENT_ID,
-//   process.env.GOOGLE_CLIENT_SECRET,
-// );
-// oauth2Client.setCredentials({
-//   refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-// });
-
-// const drive = google.drive({ version: 'v3', auth: oauth2Client });
-
-// // 複数のファイルIDを指定
-// const fileIds = JSON.parse(process.env.IMAGE_FILE_IDS);
-
-// async function downloadFile(fileId, fileName) {
-//   const dest = fs.createWriteStream(fileName);
-//   const response = await drive.files.get({ fileId, alt: 'media' }, { responseType: 'stream' });
-//   return new Promise((resolve, reject) => {
-//     response.data
-//       .on('end', () => {
-//         console.log(`Downloaded ${fileName}`);
-//         resolve();
-//       })
-//       .on('error', err => {
-//         console.error(`Error downloading file ${fileName}:`, err);
-//         reject(err);
-//       })
-//       .pipe(dest);
-//   });
-// }
-
-// (async () => {
-//   for (const { id, name } of fileIds) {
-//     await downloadFile(id, name);
-//   }
-// })();
